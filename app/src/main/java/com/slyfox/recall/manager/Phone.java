e8ac4aa4f02a2c0e1a5ba8040d72cc97cc117c79
edonoxako <edonoxako@gmail.com>
2016-10-23 12:04:42 +0700

    Sending requests to native phone call app

--- app/src/main/java/com/slyfox/recall/manager/Phone.java
+++ app/src/main/java/com/slyfox/recall/manager/Phone.java
@@ -1,6 +1,8 @@
 package com.slyfox.recall.manager;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.Toast;
 
 import com.slyfox.recall.domain.IPhone;
@@ -10,14 +12,23 @@ import com.slyfox.recall.domain.IPhone;
  */
 public class Phone implements IPhone {
 
+    public static final String URI_SCHEME_PREFIX = "tel:";
+
     private Context context;
 
     public Phone(Context context) {
         this.context = context;
     }
 
+    /**
+     * Sends request
+     * @param number ready to be sent request with phone number and service codes
+     */
     @Override
     public void makeRequest(String number) {
-        Toast.makeText(context, number, Toast.LENGTH_SHORT).show();
+        number = number.replace("*", Uri.encode("*")).replace("#", Uri.encode("#"));
+        Intent requestIntent = new Intent(Intent.ACTION_DIAL);
+        requestIntent.setData(Uri.parse(URI_SCHEME_PREFIX + number));
+        context.startActivity(requestIntent);
     }
 }
